4f3a0f70281c14d064a03a722af7238a1c1a0588
Thomas TRAN <t.tran@student.isartdigital>
2025-06-09 12:16:14 +0200

    Folders

---
+++ Projet/Scripts/Main.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+// Author : Tran Thomas
+
+namespace Com.IsartDigital.ProjectName
+{
+
+	public partial class Main : Node
+	{
+
+		#region Singleton
+		static private Main instance;
+
+		private Main() { }
+
+		static public Main GetInstance()
+		{
+			if (instance == null) instance = new Main();
+			return instance;
+
+		}
+		#endregion
+
+		public override void _Ready()
+		{
+			#region Singleton Ready
+			if (instance != null)
+			{
+				QueueFree();
+				GD.Print(nameof(Main) + " Instance already exist, destroying the last added.");
+				return;
+			}
+
+			instance = this;
+			#endregion
+
+
+		}
+
+		public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+			instance = null;
+			base.Dispose(pDisposing);
+		}
+	}
+}
